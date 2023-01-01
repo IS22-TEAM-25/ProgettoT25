c77fdd942871d143873e8d2cd022a82378f266f8
Gabriele Volani <gabrielevolani02@gmail.com>
2023-01-01 18:53:34 +0100

    update

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -17,7 +17,8 @@ Vue.prototype.$categories = [
   { title: 'per la casa', src: 'Per la casa.png', flex: 3 },
   { title: 'trasporti', src: 'Trasporti.png', flex: 3 }
 ]
-Vue.prototype.$url = "https://progettot25.up.railway.app/"
+// Vue.prototype.$url = "https://progettot25-production.up.railway.app/"
+Vue.$url = "https://progettot25.up.railway.app/"
 
 Vue.mixin({
   methods: {
