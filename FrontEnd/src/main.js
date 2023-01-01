d103e19e2fa03b97114780d123a39d027b3895d3
Gabriele Volani <gabrielevolani02@gmail.com>
2023-01-01 17:38:19 +0100

    Last preps for deployment

--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -17,7 +17,7 @@ Vue.prototype.$categories = [
   { title: 'per la casa', src: 'Per la casa.png', flex: 3 },
   { title: 'trasporti', src: 'Trasporti.png', flex: 3 }
 ]
-Vue.prototype.$url = "http://localhost:8080/"
+Vue.prototype.$url = "progettot25.up.railway.app/"
 
 Vue.mixin({
   methods: {
